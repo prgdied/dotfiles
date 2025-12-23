de6f4ce2c1ec930e53cc0514e8d6f916ae620678
prgdied <141476829+prgdied@users.noreply.github.com>
2025-12-22 20:46:42 -0500

    Delete Scripts/niri-backdrop.sh

--- Scripts/niri-backdrop.sh
+++
@@ -1,3 +0,0 @@
-echo "Starting swaybg at $(date)" >> /tmp/swaybg-debug.log
-swaybg -o '*' -i /home/payton/Pictures/Wallpapers/blackandwhiteearth.jpg >> /tmp/swaybg-debug.log 2>&1 &
-echo "swaybg PID: $!" >> /tmp/swaybg-debug.log
